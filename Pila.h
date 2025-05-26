30da076607602c45635f89a0e8d471f39e981150
Michilinto <luciana_bolanos@hotmail.com>
2025-05-25 23:24:49 -0500

    intente cambiar pila

--- Pila.h
+++ Pila.h
@@ -1,19 +1,79 @@
 #ifndef PILA_H
 #define PILA_H
+
 #include "NodoS.h"
+#include <iostream>
+#include <stdexcept>
 
 template <typename T>
-class Pila{
-    public  :
-        Pila();
-        ~ Pila();
-        void push(T movimiento);
-        void imprimir();
-        bool vacia() const;
-        T cima() const;
-        void pop();
-        
-    private :
+class Pila {
+private:
     NodoS<T>* tope;
+
+public:
+    Pila();
+    ~Pila();
+
+    void push(T movimiento);
+    void pop();
+    T cima() const;
+    bool vacia() const;
+    void imprimir() const;
 };
+
+// Implementaciones
+
+template <typename T>
+Pila<T>::Pila() {
+    tope = nullptr;
+}
+
+template <typename T>
+Pila<T>::~Pila() {
+    while (tope != nullptr) {
+        NodoS<T>* temp = tope;
+        tope = tope->siguiente;
+        delete temp;
+    }
+}
+
+template <typename T>
+void Pila<T>::push(T movimiento) {
+    NodoS<T>* nuevo = new NodoS<T>(movimiento);
+    nuevo->siguiente = tope;
+    tope = nuevo;
+}
+
+template <typename T>
+void Pila<T>::pop() {
+    if (tope != nullptr) {
+        NodoS<T>* temp = tope;
+        tope = tope->siguiente;
+        delete temp;
+    }
+}
+
+template <typename T>
+T Pila<T>::cima() const {
+    if (tope != nullptr) {
+        return tope->dato;
+    }
+    throw std::runtime_error("La pila esta vacia");
+}
+
+template <typename T>
+bool Pila<T>::vacia() const {
+    return tope == nullptr;
+}
+
+template <typename T>
+void Pila<T>::imprimir() const {
+    NodoS<T>* actual = tope;
+    while (actual != nullptr) {
+        std::cout << actual->dato << " ";
+        actual = actual->siguiente;
+    }
+    std::cout << std::endl;
+}
+
 #endif
